269d283a29330589ce2a3beacfb95e30612a6ab7
Nghia Nguyen Duc Dai <nghia.nguyenduc@axonactive.com>
2020-12-01 14:33:19 +0700

    test dn-common core

--- src/main/java/dn/com/discover/server/DiscoverServerApplication.java
+++ src/main/java/dn/com/discover/server/DiscoverServerApplication.java
@@ -1,14 +1,19 @@
 package dn.com.discover.server;
 
+import io.github.nghiandd84.core.Operation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import java.math.BigDecimal;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class DiscoverServerApplication {
 
 	public static void main(String[] args) {
+		BigDecimal result =  Operation.add(new BigDecimal(1), new BigDecimal(2));
+		System.out.println(result);
 		SpringApplication.run(DiscoverServerApplication.class, args);
 	}
 
